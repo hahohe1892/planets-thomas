4b669dae45906932b9449aead471cda5653c0447
Thomas Frank <thomas.frank@geo.uu.se>
2023-05-29 15:12:21 +0200

    this commit should be on main branch

--- code/planet_iter.py
+++ code/planet_iter.py
@@ -8,7 +8,9 @@ def acc_effect(G,M,x,y):
     return ax, ay
 
 def planet_motion(G,M,x,y,u,v,day):
-        
+    '''
+    create another test docstring
+    '''
     ax=-G*M/(abs(x**2+y**2)**[3/2])*x;
     ay=-G*M/(abs(x**2+y**2)**[3/2])*y;
     u=u+ax*day;
